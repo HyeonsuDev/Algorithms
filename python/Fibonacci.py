dfed2f7176be320312f4c76e3c64be0aa1f340df
Hyeonsu <HyeonsuKim@outlook.com>
2022-05-02 02:01:14 +0900

    Update Class

--- python/Fibonacci.py
+++ python/Fibonacci.py
@@ -1,23 +1,24 @@
-#Bottom-Up
-def fibonacci(self, n):
-    memo = {0:0, 1:1}
+class Solution:
+    #Bottom-Up
+    def fibonacci(self, n):
+        memo = {0: 0, 1: 1}
 
-    for i in range(2, n + 1):
-        memo[i] = memo[i - 1] + memo[i - 2]
-    
-    return memo[n]
+        for i in range(2, n + 1):
+            memo[i] = memo[i - 1] + memo[i - 2]
 
-#Top-Down with Memo
-def fibonacci_memo(self, n):
-
-    memo = {}
-    if n in [0, 1]:
-        return n
-
-    if n in memo:
         return memo[n]
 
-    memo[n] = fibonacci_memo(n - 1) + fibonacci_memo(n - 2)
-    return memo[n]
+    #Top-Down with Memo
+    def fibonacci_memo(self, n):
 
-#With Matrix
+        memo = {}
+        if n in [0, 1]:
+            return n
+
+        if n in memo:
+            return memo[n]
+
+        memo[n] = self.fibonacci_memo(n - 1) + self.fibonacci_memo(n - 2)
+        return memo[n]
+
+    #With Matrix
